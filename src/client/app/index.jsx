c13035d36286afc3454f9b1c9c2f4717159c6d54
Brandon Wong <b.lh.wong@gmail.com>
2017-03-20 11:30:50 -0700

    Merge conflicts from Jon

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component {
   render () {
     return (
       <div>
         <p> Hello Viz.ly!</p>
+        <form ref='uploadForm'
+          id='uploadForm'
+          action='http://localhost:3000/upload'
+          method='post'
+          encType="multipart/form-data">
+          <input type="file" name="sampleFile" />
+          <input type='submit' value='Upload!' />
+        </form>
       </div>
     );
   }
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,35 @@
 var app = require('./app.js');
 var db = require('./db/db.js');
+var fileUpload = require('express-fileUpload');
+var Vision = require('@google-cloud/vision');
+var google = require('googleapis');
+var GoogleAuth = require('google-auth-library');
 
 
-app.listen(3000, function() {
-  console.log('Server listening.  Go Vizly');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+app.use('/', express.static(__dirname + '/../client'));
+
+
+app.post('/upload', function(req, res) {
+  console.log('in upload!');
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded...');
+  }
+
+  var sampleFile = req.files.sampleFile;
+  sampleFile.mv(__dirname + '/db/hi.jpg', function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send('File uploaded!');
+  });
+
 });
+
 
 
 // app.get('/testget', function(req, res) {
@@ -15,4 +40,9 @@ app.listen(3000, function() {
 
 //For get/set routes go to routes.js
 
+
+
+app.listen(3000, function() {
+  console.log('Server listening.  Go Vizly');
+});
 
