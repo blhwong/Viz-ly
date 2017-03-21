1414da8ea2b2794194ef1f3691e309d7bf37997a
Brandon Wong <b.lh.wong@gmail.com>
2017-03-21 15:14:32 -0700

    Test receiving data from post

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,6 +3,24 @@ import {render} from 'react-dom';
 import $ from 'jquery';
 
 class App extends React.Component {
+  handleUpload(e) {
+    debugger;
+    // e.preventDefault();
+    console.log('handle upload!');
+    // $.ajax({
+    //   url: '/upload',
+    //   type: 'POST',
+    //   cache: false,
+    //   // contentType: 'application/json',
+    //   data: $('form').serialize(),
+    //   success: function(data) {
+    //     console.log(data);
+    //   },
+    //   error: function() {
+    //     console.log('error');
+    //   }
+    // });
+  }
   render () {
     return (
       <div>
@@ -13,7 +31,7 @@ class App extends React.Component {
           method='post'
           encType="multipart/form-data">
           <input type="file" name="sampleFile" multiple/>
-          <input type='submit' value='Upload!' />
+          <input onClick={this.handleUpload} type='submit' value='Upload!' />
         </form>
         <h5>Please upload photos under 4MB!</h5>
       </div>
